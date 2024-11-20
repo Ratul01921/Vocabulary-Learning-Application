08e2a583c4add5387c265655fa217ac4bb5833d9
Shahriar Ratul <shahriarratul01921@gmail.com>
2024-11-20 14:50:31 +0600

    react router setup done

---
+++ src/Layout/HomeLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <h1 className='btn text-red-700'>this is Home Layout</h1>
+        </div>
+    );
+};
+
+export default HomeLayout;
---
+++ src/Routes/router.jsx
@@ -0,0 +1,15 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import HomeLayout from "../Layout/HomeLayout";
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeLayout></HomeLayout>,
+    },
+  ]);
+  
+export default router;
